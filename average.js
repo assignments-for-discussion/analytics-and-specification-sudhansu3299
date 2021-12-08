84c66ecbeb6ed1fdc3609acc1b0e688249007893
S^3 <43723729+sudhansu3299@users.noreply.github.com>
2021-12-08 22:13:27 +0530

    updating for corner cases

--- average.js
+++ average.js
@@ -1,6 +1,8 @@
 
 function average(numbers) {
-  if(numbers.length>0 && !(numbers.some(isNaN)))
+  if(numbers.length == 0)
+    return NaN;
+  else if(!(numbers.some(isNaN)))
     return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
 }
 
