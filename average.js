e2e56442a4a70c07ab2cff0c4d7749c4f86ea543
S^3 <43723729+sudhansu3299@users.noreply.github.com>
2021-12-08 22:46:36 +0530

    update corner #3

--- average.js
+++ average.js
@@ -2,7 +2,7 @@
 function average(numbers) {
   if(numbers.some(isNaN) || isNaN(numbers))
     return;
-  else if(numbers.length == 0)
+  if(numbers.length == 0)
     return NaN;
   else if(!(numbers.some(isNaN)))
     return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
