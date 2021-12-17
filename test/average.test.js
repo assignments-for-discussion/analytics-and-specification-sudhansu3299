87c7f885f7dea9e9e5fa4d42eeb9b3317df05929
S^3 <43723729+sudhansu3299@users.noreply.github.com>
2021-12-17 16:30:51 +0530

    Updating the test comment #1
    To reduce the comment to 80 Lines Max

--- test/average.test.js
+++ test/average.test.js
@@ -15,7 +15,7 @@ it('ignores NaN in the input', ()=> {
   expect(average([1, NaN, 2])).to.be.approximately(1.5, 0.01);
 });
 
-//assuming thresholds for battery operation when battery_temp> 5 deg C and battery_temp<45 deg C
+//assuming thresholds for battery opn. when temp> 5 deg C and temp<45 deg C
 it('ingores outliers in the input which are outside the thresholds',()=> {
   expect(average([6,-3,10,50])).to.be.approximately(8, 0.01);;
 });
